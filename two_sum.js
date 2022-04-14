fdd786c3ccf7a6a78aa639a9977ccdb183c0199b
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-14 09:53:51 -0500

    Add O(N) solutiuon
    Use Hash table to increase lookup performance

--- two_sum.js
+++ two_sum.js
@@ -6,14 +6,32 @@
 
 // https://leetcode.com/problems/two-sum/
 
-var twoSum = function(nums, target) {
+let n_square_solution = (nums, target) => {
   let auxiliar = nums.map((element) => target - element);
   for (let index = 0; index < nums.length; index++) {
     let element = auxiliar[index];
     presence = nums.indexOf(element);
     console.log(presence);
     if (presence > -1 && presence !== index) {
-      return [presence, index]
+      return [presence, index];
     } 
   }
+}
+
+let n_solution = (nums, target) => {
+  let map = new Map
+  for (let index = 0; index < nums.length; index++) {
+    let element = nums[index];
+    let searchable = target - element;
+    
+    if (map.has(searchable)) {
+      return [map.get(searchable), index];
+    }
+    
+    map.set(element, index);
+  }
+}
+
+var twoSum = function(nums, target) {
+  return n_solution(nums, target);
 };
