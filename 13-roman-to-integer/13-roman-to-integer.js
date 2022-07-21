74f5fc10678f625e76ad6ab517f1dfedecba4aba
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-07-21 16:22:26 -0500

    Time: 191 ms (61.52%), Space: 50.1 MB (12.30%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -17,20 +17,12 @@ var romanToInt = function(s) {
     
     for (let i = 0; i < s.length; i++) {
         let num = dic[s[i]];
-        const nextNum = dic[s[i+1]];
+        const nextNum = s[i+1];
         
         
-        if (s[i] === 'I' && (s[i+1] === 'V' || s[i+1] === 'X')) {
+        if (nextNum && (s[i] + nextNum).match(/(I[VX])|(X[LC])|(C[DM])/)) {
             num = -num;
-            num += nextNum;
-            i++;
-        } else if (s[i] === 'X' && (s[i+1] === 'L' || s[i+1] === 'C')) {
-            num = -num;
-            num += nextNum;
-            i++;
-        } else if (s[i] === 'C' && (s[i+1] === 'D' || s[i+1] === 'M')) {
-            num = -num;
-            num += nextNum;
+            num += dic[nextNum];
             i++;
         }
         
