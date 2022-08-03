3f120ea37597bb47edfaac7e4681a8e4cdf38bca
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-03 17:13:14 -0500

    Time: 84 ms (96.27%), Space: 44.4 MB (79.85%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+  const wordsMap = new Map();
+  ransomNote.split("").forEach((char) => {
+    const wordCounted = wordsMap.get(char)
+    if (wordCounted) {
+      wordsMap.set(char, wordCounted + 1);
+    } else {
+      wordsMap.set(char, 1);
+    }     
+  });
+  for(let i=0; i < magazine.length; i++) {
+    const char = magazine[i];
+    let mapCount = wordsMap.get(char)
+    if (mapCount) {
+      mapCount -= 1;
+      if (mapCount === 0) {
+        wordsMap.delete(char);  
+      } else {
+        wordsMap.set(char, mapCount);
+      }
+    }
+    if (wordsMap.size === 0) {
+      return true;
+    }
+  }
+  return false;
+};
